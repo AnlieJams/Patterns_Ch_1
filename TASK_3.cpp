e0041d89f15f22d1f461c7eed7ddb9d82b0a0b62
AnlieJams <98762736+AnlieJams@users.noreply.github.com>
2023-06-03 00:13:44 +0300

    Add files via upload

---
+++ TASK_3.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//Задание 3: Контейнеры для чисел
+
+
+    struct AddConstant {
+    int constant;
+
+    
+    AddConstant(int value) : constant(value) {}
+
+  
+    void operator()(int& value) const {
+        value += constant;
+    }
+};
+
+int main() {
+    
+    vector<int> myVector = { 1, 2, 3, 4, 5 };
+
+    cout << "Исходный вектор: ";
+    for (int num : myVector) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    
+    AddConstant addConstant(3);
+
+    
+    for_each(myVector.begin(), myVector.end(), addConstant);
+
+    
+    cout << "Измененный вектор: ";
+    for (int num : myVector) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
